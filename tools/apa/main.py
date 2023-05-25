c05130b1dd1d4c015937e36da0aa7c94de961c9d
xjli32 <xjli32@iflytek.com>
2023-05-25 09:58:51 +0800

    add apa planning tools

---
+++ tools/apa/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+from plot_planning_info import PlotPlanningInfo
+
+if __name__ == '__main__':
+    data_path = "" # change to log file path
+    plot_all_frame = PlotPlanningInfo(data_path)
+    plot_all_frame.read_file()
+    plot_all_frame.plot_pos_data()
+    plot_all_frame.plot_time_diff()
