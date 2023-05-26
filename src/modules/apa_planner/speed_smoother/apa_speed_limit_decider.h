065134204f2bc6f5bc5f432c62cfb1fbc69979d7
xjli32 <xjli32@iflytek.com>
2023-05-26 17:26:16 +0800

    add apa speed limit decider

---
+++ src/modules/apa_planner/speed_smoother/apa_speed_limit_decider.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+#include "planning_plan.pb.h"
+
+#include "apa_planner/common/apa_speed_limit.h"
+#include "common/math/line_segment2d.h"
+
+namespace planning {
+
+class ApaSpeedLimitDecider {
+ public:
+  ApaSpeedLimitDecider() = default;
+
+  ~ApaSpeedLimitDecider() = default;
+
+  bool GetSpeedLimits(
+      const ::PlanningOutput::PlanningOutput& planning_output,
+      const std::vector<planning_math::LineSegment2d>& obstacles,
+      ApaSpeedLimit* const speed_limit_data) const;
+
+ private:
+  double CalSpeedLimitFromObstacleDistance(
+      const double dis_from_obstacle) const;
+};
+
+}  // namespace planning
