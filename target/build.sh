f8cd4650771cfe05ca18893676bda1127de6c16b
xjli32 <xjli32@iflytek.com>
2023-06-16 13:53:12 +0800

    1. update build.sh; 2. update CHANGELOG.md; 3. modify CMakeLists.txt;

--- target/build.sh
+++ target/build.sh
@@ -1,29 +1,9 @@
-cd ..
-# 创建output目录
-OutputDir="install/Planning"
-if [ -d $OutputDir ]; then rm -rf $OutputDir; fi
-mkdir $OutputDir
-OutputLibDir="install/Planning/Lib"
-if [ -d $OutputLibDir ]; then rm -rf $OutputLibDir; fi
-mkdir $OutputLibDir
+#!/bin/bash
 
-# 创建build目录
-BuildDir="build"
-if [ -d $BuildDir ]; then rm -rf $BuildDir; fi
-mkdir $BuildDir
-cd $BuildDir
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR/..
 
-cmake ../
-if [ "$?" -ne 0 ]; then echo "cmake failed"; exit 1; fi
-make
-if [ "$?" -ne 0 ]; then echo "make failed"; exit 1; fi
-make install
-if [ "$?" -ne 0 ]; then echo "make install failed"; exit 1; fi
-
-cd ..
-
-cp target/README.txt install/Interface/
-cp CHANGELOG.MD install/Interface/
-cp -r doc install/Interface/Doc
-
-cd target/
+mkdir build
+cd build
+cmake ..
+make -j && make install
