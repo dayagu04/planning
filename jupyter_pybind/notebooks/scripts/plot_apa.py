4e48d0d82220356d95523fbd1e358b0e789732ec
xlwang71 <xlwang71@iflytek.com>
2023-10-09 11:34:03 +0800

    feat: update plot apa

--- jupyter_pybind/notebooks/scripts/plot_apa.py
+++ jupyter_pybind/notebooks/scripts/plot_apa.py
@@ -6,18 +6,19 @@ sys.path.append('../..')
 sys.path.append('../../../')
 
 # bag path and frame dt
-bag_path = '/pnc_x86_ywwang33/dataAnalysis/test_7.00000'
+bag_path = '/home/xlwang71/Downloads/APA/0924/test_9.00000'
 frame_dt = 0.1 # sec
-parking_flag = True
-plot_ctrl_flag = True
+plot_ctrl_flag = False
 
 display(HTML("<style>.container { width:95% !important;  }</style>"))
 output_notebook()
 
-bag_loader = LoadCyberbag(bag_path, parking_flag)
+bag_loader = LoadCyberbag(bag_path, True)
 max_time = bag_loader.load_all_data()
 fig1, local_view_data = load_local_view_figure_parking()
-fig2, fig3, fig4, fig5, fig6, fig7, data_ctrl_debug_table = load_local_view_figure_parking_ctrl(bag_loader, local_view_data)
+
+if plot_ctrl_flag:
+  fig2, fig3, fig4, fig5, fig6, fig7, data_ctrl_debug_table = load_local_view_figure_parking_ctrl(bag_loader, local_view_data)
 
 ### sliders config
 class LocalViewSlider:
@@ -30,14 +31,12 @@ class LocalViewSlider:
 def slider_callback(bag_time):
   kwargs = locals()
   update_local_view_data_parking(fig1, bag_loader, bag_time, local_view_data, plot_ctrl_flag)
-  # plan_msg_idx = 0
-  # if bag_loader.plan_msg['enable'] == True:
-  #   while bag_loader.plan_msg['t'][plan_msg_idx] <= bag_time and plan_msg_idx < (len(bag_loader.plan_msg['t'])-2):
-  #       plan_msg_idx = plan_msg_idx + 1
-
-  # bag_loader.plan_msg['data'][plan_msg_idx]
 
   push_notebook()
 
-bkp.show(row(fig1, column(fig2, fig3, fig4, fig5), column(fig6, fig7, data_ctrl_debug_table)), notebook_handle=True)
+if plot_ctrl_flag:
+  bkp.show(row(fig1, column(fig2, fig3, fig4, fig5), column(fig6, fig7, data_ctrl_debug_table)), notebook_handle=True)
+else:
+  bkp.show(fig1, notebook_handle=True)
+
 slider_class = LocalViewSlider(slider_callback)
