d40ee371b3d0e9acb59d4877467b9bd1bf720e31
xykuai <xykuai@iflytek.com>
2024-05-08 11:37:00 +0800

    Add plot_vo_lat_behavior_html.py generation script

--- jupyter_pybind/notebooks_scc/scripts/html_generator.py
+++ jupyter_pybind/notebooks_scc/scripts/html_generator.py
@@ -13,6 +13,8 @@ def run_command_by_path(file_path):
         run_command = ["python", "plot_lon_plan_html.py", file_path]
     elif command == "plot_apa_html":
         run_command = ["python", "plot_apa_html.py", file_path]
+    elif command == "plot_vo_lat_behavior_html":
+        run_command = ["python", "plot_vo_lat_behavior_html.py", file_path]
     else:
         print(f"未知的命令: {command}")
         return
