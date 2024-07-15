0916561fe0c2e0aa2a6b7e8ff2ae53209bdeb0f3
fengwang31 <fengwang31@iflytek.com>
2024-07-15 20:28:00 +0800

    modify proto_gen.py

--- jupyter_pybind/scripts/proto_gen.py
+++ jupyter_pybind/scripts/proto_gen.py
@@ -7,17 +7,32 @@ def proto_gen_py():
         print(f"python_protos already exist")
         shutil.rmtree("../python_proto")
 
-
     os.mkdir("../python_proto")
     current_dir = os.getcwd()
     new_dir = os.path.join(current_dir, "../python_proto")
-
     os.chdir("../../proto")
     proto_lists = os.listdir(".")
+
     for i in range(len(proto_lists)):
         if ".proto" in proto_lists[i]:
-            command = f"protoc --python_out={new_dir} {proto_lists[i]}"
-            os.system(command)
+          command = f"protoc --python_out={new_dir} {proto_lists[i]}"
+          os.system(command)
+    
+    os.chdir(current_dir)
+    if os.path.exists("../../interface"):
+      if "ehr_sdmap.proto" in os.listdir("../../interface/src/proto/"):
+        os.chdir("../../interface/src/proto/")
+        proto_lists.append("ehr_sdmap.proto")
+        command = f"protoc --python_out={new_dir} {proto_lists[-1]}"
+        os.system(command)
+    elif os.path.exists("../../../interface"):
+      if "ehr_sdmap.proto" in os.listdir("../../../interface/src/proto/"):
+        os.chdir("../../../interface/src/proto/")
+        proto_lists.append("ehr_sdmap.proto")
+        command = f"protoc --python_out={new_dir} {proto_lists[-1]}"
+        os.system(command)
+        proto_lists.append("ehr_sdmap.proto")
+
     return 0
 
 if __name__ == "__main__":
