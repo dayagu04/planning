05799649997651b9741e76e09ff512a05149c1ec
xykuai <xykuai@iflytek.com>
2024-07-31 11:38:00 +0800

    add overtake lc surppression nearby route end

--- src/modules/tasks/behavior_planners/lane_change_decider/lane_change_request_manager.cc
+++ src/modules/tasks/behavior_planners/lane_change_decider/lane_change_request_manager.cc
@@ -57,8 +57,9 @@ bool LaneChangeRequestManager::Update(
   double minimum_distance_nearby_ramp_to_surpress_overtake_lane_change =
       config_.minimum_distance_nearby_ramp_to_surpress_overtake_lane_change;
   const double max_pass_merge_distance_to_surpress_overtake_lane_change = 
-      virtual_lane_mgr_->pass_merge_point_dis_threshold_for_ramp_lane_merge_to_road_lane();
+      virtual_lane_mgr_->dis_threshold_to_last_merge_point();
   double sum_dis_to_last_merge_point = virtual_lane_mgr_->sum_dis_to_last_merge_point();
+  const double odd_route_distance_threshold = 500.0;
 
   int state = lane_change_decider_output.curr_state;
   if (int_request_.enable_int_request() || enable_mrc_pull_over) {
@@ -97,6 +98,12 @@ bool LaneChangeRequestManager::Update(
           sum_dis_to_last_merge_point < max_pass_merge_distance_to_surpress_overtake_lane_change) {
         overtake_request_.Reset();
         LOG_DEBUG("cann't generate overtake lane change on ramp or near ramp or near merge");
+        EnableGenerateOvertakeQequestByFrontSlowVehicle = false;
+      }
+
+      if (virtual_lane_mgr_->get_distance_to_route_end() < odd_route_distance_threshold) {
+        overtake_request_.Reset();
+        LOG_DEBUG("cann't generate overtake lane change nearby odd boundary");
         EnableGenerateOvertakeQequestByFrontSlowVehicle = false;
       }
 
