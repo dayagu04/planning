63983f1afed5592435b44c26844e21e4b9009cfb
slruan2 <slruan2@iflytek.com>
2024-08-16 15:54:04 +0800

    add stopline and zebra_crossing_line

--- jupyter_pybind/notebooks_scc/scripts/plot_lon_plan.py
+++ jupyter_pybind/notebooks_scc/scripts/plot_lon_plan.py
@@ -7,7 +7,7 @@ sys.path.append('../..')
 sys.path.append('../../../')
 
 # bag path and frame dt
-bag_path = "/data_cold/abu_zone/autoparse/chery_e0y_10034/trigger/20240731/20240731-10-42-34/data_collection_CHERY_E0Y_10034_EVENT_MANUAL_2024-07-31-10-42-34_no_camera.bag"
+bag_path = "/data_cold/abu_zone/autoparse/chery_e0y_13484/trigger/20240815/20240815-17-15-29/data_collection_CHERY_E0Y_13484_EVENT_MANUAL_2024-08-15-17-15-29_no_camera.bag"
 frame_dt = 0.1 # sec
 
 display(HTML("<style>.container { width:95% !important;  }</style>"))
