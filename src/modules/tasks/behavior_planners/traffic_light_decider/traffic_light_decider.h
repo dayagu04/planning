f59b93694b8eb4da72c6b94bfddf6b781a32b2f4
gpxu <gpxu@iflytek.com>
2024-09-11 09:46:09 +0800

    adapt new tfl interface and add blink logic

--- src/modules/tasks/behavior_planners/traffic_light_decider/traffic_light_decider.h
+++ src/modules/tasks/behavior_planners/traffic_light_decider/traffic_light_decider.h
@@ -25,5 +25,9 @@ class TrafficLightDecider : public Task {
   TrafficLightDeciderConfig config_;
   bool is_first_car_ = false;
   bool is_pass_stopline_ = false;
+  bool can_pass_ = true;
+  double green_light_timer_ = 0.0;
+  double green_blink_timer_ = 0.0;
+  double yellow_light_timer_ = 0.0;
 };
 }  // namespace planning
