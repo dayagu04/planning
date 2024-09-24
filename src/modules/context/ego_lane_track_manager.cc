c42b4e19963994c5ab304670a9fc7b8d71971bde
xykuai <xykuai@iflytek.com>
2024-09-24 09:51:50 +0800

    [fix]lcc split select for not in the extreme lane

--- src/modules/context/ego_lane_track_manager.cc
+++ src/modules/context/ego_lane_track_manager.cc
@@ -1172,19 +1172,32 @@ void EgoLaneTrackManger::PreprocessIntersectionSplit(
   }
 
   if (!is_exist_split_on_intersection_) {
-    bool is_on_right_side_lane = true;
+    bool is_on_right_side_lane = false;
+    bool is_on_left_side_lane = false;
     for (size_t i = 0; i < order_ids.size(); i++) {
       if (order_ids[i] == 0) {
-        is_on_right_side_lane = false;
+        is_on_left_side_lane = true;
         break;
       }
     }
-    if (!is_on_right_side_lane) {
+    for (size_t i = 0; i < order_ids.size(); i++) {
+      if (order_ids[i] == relative_id_lanes.size() - 1) {
+        is_on_right_side_lane = true;
+        break;
+      }
+    }    
+    if (is_on_left_side_lane && is_on_right_side_lane) {
+      is_exist_split_on_intersection_= false;
+      return;
+    } else if (is_on_left_side_lane) {
       relative_id_lanes[order_ids[1]]->set_relative_id(0);
       origin_order_id = relative_id_lanes[order_ids[1]]->get_order_id();
-    } else {
+    } else if (is_on_right_side_lane) {
       relative_id_lanes[order_ids[0]]->set_relative_id(0);
       origin_order_id = relative_id_lanes[order_ids[0]]->get_order_id();    
+    } else {
+      is_exist_split_on_intersection_= false;
+      return;      
     }
   
     is_exist_split_on_intersection_ = true;
