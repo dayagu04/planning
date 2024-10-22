31b638ea88cad0f559638165999bbe939d0916e0
ymwu11 <ymwu11@iflytek.com>
2024-10-22 16:18:50 +0800

    add slot

--- src/library/occupancy_grid_map/virtual_wall_decider.cc
+++ src/library/occupancy_grid_map/virtual_wall_decider.cc
@@ -189,6 +189,8 @@ int VirtualWallDecider::Process(std::vector<Position2D>& points,
   }
 
   else {
+    CalcParallelVirtualWall(points, channel_length, channel_width, slot_width,
+                            slot_length, ego_pose, end);
   }
 
   return 0;
@@ -230,6 +232,143 @@ void VirtualWallDecider::GenerateCarRelativePosition(const Pose2D& ego_pose) {
 }
 
 void VirtualWallDecider::CalcVerticalVirtualWall(
+    std::vector<Position2D>& points, const double channel_length,
+    const double channel_width, const double slot_width,
+    const double slot_length, const Pose2D& ego_pose, const Pose2D& end) {
+  // width is the slot upper edge to a virtual wall
+  const double lower_channel_length = (channel_length - slot_width) * 0.5;
+
+  // right slot wall
+  Eigen::Vector2d right_wall_start =
+      Eigen::Vector2d(0, -slot_width / 2.0 - SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  Eigen::Vector2d right_wall_end =
+      Eigen::Vector2d(slot_length - SLOT_VIRTUAL_WALL_X_OFFSET,
+                      -slot_width / 2.0 - SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  // left slot wall
+  Eigen::Vector2d left_wall_start =
+      Eigen::Vector2d(0, slot_width / 2.0 + SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  Eigen::Vector2d left_wall_end =
+      Eigen::Vector2d(slot_length - SLOT_VIRTUAL_WALL_X_OFFSET,
+                      slot_width / 2.0 + SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  // right channel
+  const Eigen::Vector2d right_channel_end =
+      Eigen::Vector2d(slot_length - SLOT_VIRTUAL_WALL_X_OFFSET,
+                      -slot_width / 2.0 - lower_channel_length);
+
+  // left channel
+  const Eigen::Vector2d left_channel_end =
+      Eigen::Vector2d(slot_length - SLOT_VIRTUAL_WALL_X_OFFSET,
+                      slot_width / 2.0 + lower_channel_length);
+
+  // channel up bound
+  double channel_up_bound_x;
+  double veh_up_x = ego_pose.x + 5.0 * std::fabs(std::cos(ego_pose.theta));
+  channel_up_bound_x = veh_up_x + 8.0;
+
+  // limit upper
+  if (channel_up_bound_x > slot_length + 9.0) {
+    channel_up_bound_x = slot_length + 9.0;
+  }
+
+  // limit lower
+  if (channel_up_bound_x < veh_up_x + 3.0) {
+    channel_up_bound_x = veh_up_x + 3.0;
+  }
+
+  Eigen::Vector2d upper_channel_left = Eigen::Vector2d(
+      channel_up_bound_x, slot_width / 2.0 + lower_channel_length);
+  Eigen::Vector2d upper_channel_right = Eigen::Vector2d(
+      channel_up_bound_x, -slot_width / 2.0 - lower_channel_length);
+
+  // channel right virtual wall
+
+  Eigen::Vector2d right_channel_lower;
+  Eigen::Vector2d right_channel_upper;
+
+  double theta = IflyUnifyTheta(ego_pose.theta, M_PI);
+  double channel_right_bound_y;
+  double channel_left_bound_y;
+
+  double tail_buffer = 5.5;
+  double head_buffer = 7.0;
+
+  if (relative_position_ == CarSlotRelativePosition::car_is_right) {
+    if (theta < 0.0) {
+      channel_right_bound_y = ego_pose.y - head_buffer;
+      channel_left_bound_y = slot_width / 2.0 + 5.0;
+    } else if (theta > 0.0) {
+      channel_right_bound_y = ego_pose.y - tail_buffer;
+      channel_left_bound_y = slot_width / 2.0 + head_buffer;
+    } else {
+      channel_right_bound_y = -slot_width / 2.0 - lower_channel_length;
+      channel_left_bound_y = slot_width / 2.0 + 10.0;
+    }
+  } else if (relative_position_ == CarSlotRelativePosition::car_is_left) {
+    if (theta < 0.0) {
+      channel_right_bound_y = -slot_width / 2 - head_buffer;
+      channel_left_bound_y = ego_pose.y + tail_buffer;
+    } else if (theta > 0.0) {
+      channel_right_bound_y = -slot_width / 2 - tail_buffer;
+
+      channel_left_bound_y = ego_pose.y + head_buffer;
+    } else {
+      channel_right_bound_y = -slot_width / 2.0 - 12.0;
+
+      channel_left_bound_y = slot_width / 2 + 12.0;
+    }
+  } else {
+    channel_right_bound_y = -slot_width / 2.0 - 12.0;
+    channel_left_bound_y = slot_width / 2 + 12.0;
+  }
+
+  right_channel_lower = Eigen::Vector2d(
+      slot_length - SLOT_VIRTUAL_WALL_X_OFFSET, channel_right_bound_y);
+  right_channel_upper =
+      Eigen::Vector2d(slot_length + channel_width, channel_right_bound_y);
+
+  // channel left virtual wall
+  Eigen::Vector2d left_channel_lower;
+  Eigen::Vector2d left_channel_upper;
+
+  left_channel_lower = Eigen::Vector2d(slot_length - SLOT_VIRTUAL_WALL_X_OFFSET,
+                                       channel_left_bound_y);
+  left_channel_upper =
+      Eigen::Vector2d(slot_length + channel_width, channel_left_bound_y);
+
+  // slot back wall
+  double lower_bound_x = -1.0;
+  Eigen::Vector2d back_wall_left = Eigen::Vector2d(
+      lower_bound_x, -slot_width / 2.0 - SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  Eigen::Vector2d back_wall_right = Eigen::Vector2d(
+      lower_bound_x, slot_width / 2.0 + SLOT_VIRTUAL_WALL_Y_OFFSET);
+
+  // add points
+  SampleInLine(right_wall_start, right_wall_end, &points);
+
+  SampleInLine(left_wall_start, left_wall_end, &points);
+
+  SampleInLine(right_wall_end, right_channel_end, &points);
+
+  SampleInLine(left_wall_end, left_channel_end, &points);
+
+  SampleInLine(upper_channel_left, upper_channel_right, &points);
+
+  SampleInLine(right_channel_lower, right_channel_upper, &points);
+
+  SampleInLine(left_channel_lower, left_channel_upper, &points);
+
+  SampleInLine(back_wall_left, back_wall_right, &points);
+
+  return;
+}
+
+
+void VirtualWallDecider::CalcParallelVirtualWall(
     std::vector<Position2D>& points, const double channel_length,
     const double channel_width, const double slot_width,
     const double slot_length, const Pose2D& ego_pose, const Pose2D& end) {
--- src/library/occupancy_grid_map/virtual_wall_decider.h
+++ src/library/occupancy_grid_map/virtual_wall_decider.h
@@ -36,6 +36,13 @@ class VirtualWallDecider {
                                const double slot_length, const Pose2D& ego_pose,
                                const Pose2D& end);
 
+  void CalcParallelVirtualWall(std::vector<Position2D>& points,
+                               const double channel_length,
+                               const double channel_width,
+                               const double slot_width,
+                               const double slot_length, const Pose2D& ego_pose,
+                               const Pose2D& end);
+
   std::string name_;
   Pose2D start_;
   Pose2D end_;
