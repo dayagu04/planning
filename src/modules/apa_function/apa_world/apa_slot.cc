8db9909750e4ae6728147464ef9f1274ab2035c6
ymwu11 <ymwu11@iflytek.com>
2024-12-06 20:37:44 +0800

    [fix] fix compile warning

--- src/modules/apa_function/apa_world/apa_slot.cc
+++ src/modules/apa_function/apa_world/apa_slot.cc
@@ -2,5 +2,29 @@
 
 namespace planning {
 
-void ApaSlot::Init() { return; }
+ApaSlot::ApaSlot() {}
+
+void ApaSlot::Init() {
+  slot_type = SlotType::INVALID;
+  slot_side = SlotSide::INVALID;
+  perception_id_ = 0;
+  height_top_ = 0;
+  height_bottom_ = 0;
+  perception_source_type_ = iflyauto::SlotSourceType::SLOT_SOURCE_TYPE_INVALID;
+
+  is_perception_release_ = false;
+  slot_material_type_ = SlotMaterialType::UNKOWN;
+
+  heading_ = 0.0;
+  id_ = 0;
+  is_planning_release_ = false;
+  is_suggested_slot_ = false;
+
+  ego_occupied_ratio_ = 0;
+
+  release_info_.Clear();
+  obs_info_.Clear();
+
+  return;
+}
 }  // namespace planning
