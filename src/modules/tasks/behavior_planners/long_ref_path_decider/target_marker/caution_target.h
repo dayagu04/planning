6d5a662aca4fdd5da55603f02a8166c2249e7e5a
gpxu <gpxu@iflytek.com>
2024-12-09 09:53:52 +0800

    add overtake and caution targets

---
+++ src/modules/tasks/behavior_planners/long_ref_path_decider/target_marker/caution_target.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "target.h"
+#include "ego_planning_config.h"
+
+namespace planning {
+
+class CautionTarget : public Target {
+
+  struct UpperBoundInfo {
+    double s = 0.0;
+    double t = 0.0;
+    double v = 0.0;
+    TargetType target_type = TargetType::kNotSet;
+    int32_t agent_id = -1;
+  };
+
+public:
+  CautionTarget(const SpeedPlannerConfig& config, framework::Session* session);
+  ~CautionTarget() = default;
+
+private:
+  void GenerateUpperBoundInfo();
+
+  void GenerateCautionTarget();
+
+private:
+  std::vector<UpperBoundInfo> upper_bound_infos_;
+};
+
+} // namespace planning
