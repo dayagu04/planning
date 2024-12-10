460165a27fccf2a48cea27c4d3331abb3f31fb23
clzhao9 <clzhao9@iflytek.com>
2024-12-10 10:48:56 +0800

    only borrow from another direction far from blocking area

--- src/modules/tasks/behavior_planners/lane_borrow_decider/lane_borrow_deciderv1.cc
+++ src/modules/tasks/behavior_planners/lane_borrow_decider/lane_borrow_deciderv1.cc
@@ -384,54 +384,72 @@ bool LaneBorrowDecider::ObstacleDecision() {
 
   const auto& front_obstacle_sl =
       static_blocked_obstacles_[0]->frenet_obstacle_boundary();
-  if (!left_borrow_ && !right_borrow_) {
-    bypass_direction_ = 0;
-    lane_borrow_decider_output_.lane_borrow_failed_reason =
-        LANE_TYPE_CHECK_FAILED;
-    return false;
-  } else if (left_borrow_ && !right_borrow_) {
+  // if (!left_borrow_ && !right_borrow_) {
+  //   bypass_direction_ = 0;
+  //   lane_borrow_decider_output_.lane_borrow_failed_reason =
+  //       LANE_TYPE_CHECK_FAILED;
+  //   return false;
+  // } else if (left_borrow_ && !right_borrow_) {
+  //   bypass_direction_ = 1;
+  // } else if (!left_borrow_ && right_borrow_) {
+  //   bypass_direction_ = 2;
+  // } else {
+  //   if (front_obstacle_sl.l_start * front_obstacle_sl.l_end <= 0) {
+  //     bypass_direction_ = 0;
+  //     lane_borrow_decider_output_.lane_borrow_failed_reason = CENTER_OBSTACLE;
+  //     return false;
+  //   } else {
+  //     bypass_direction_  = GetBypassDirection(front_obstacle_sl);
+  //   }
+  // }
+
+  int front_obs_bypass_direction  = GetBypassDirection(front_obstacle_sl);
+  if (front_obs_bypass_direction == 1 && left_borrow_){
     bypass_direction_ = 1;
-  } else if (!left_borrow_ && right_borrow_) {
+  }else if(front_obs_bypass_direction == 2 && right_borrow_){
     bypass_direction_ = 2;
-  } else {
-    if (front_obstacle_sl.l_start * front_obstacle_sl.l_end <= 0) {
-      bypass_direction_ = 0;
-      lane_borrow_decider_output_.lane_borrow_failed_reason = CENTER_OBSTACLE;
-      return false;
-    } else {
-      bypass_direction_  = GetBypassDirection(front_obstacle_sl);
-    }
+  }else {
+    bypass_direction_ = 0;
+    lane_borrow_decider_output_.lane_borrow_failed_reason = CENTER_OBSTACLE;
+    return false;
   }
 
   for (const auto& obstacle : static_blocked_obstacles_) {
     const auto& id = obstacle->obstacle()->id();
     const auto& frenet_obstacle_sl = obstacle->frenet_obstacle_boundary();
+    int obs_bypass_direction = GetBypassDirection(frenet_obstacle_sl);
 
-    if (frenet_obstacle_sl.l_start * frenet_obstacle_sl.l_end <= 0) {
+    if (obs_bypass_direction == 0){
       bypass_direction_ = 0;
       lane_borrow_decider_output_.lane_borrow_failed_reason = CENTER_OBSTACLE;
       return false;
-    }
-
-    int obs_bypass_direction = GetBypassDirection(frenet_obstacle_sl);
-
-    if (left_borrow_ && right_borrow_) {
-      if (obs_bypass_direction == bypass_direction_) {
+    }else if (obs_bypass_direction == bypass_direction_) {
         obs_left_l_ = std::max(obs_left_l_, frenet_obstacle_sl.l_end);
         obs_right_l_ = std::min(obs_right_l_, frenet_obstacle_sl.l_start);
         obs_start_s_ = std::min(obs_start_s_, frenet_obstacle_sl.s_start);
         obs_end_s_ = std::max(obs_end_s_, frenet_obstacle_sl.s_end);
         static_blocked_obj_vec_.emplace_back(obstacle->obstacle()->id());
-      } else {
-        break;
-      }
     } else {
-      obs_left_l_ = std::max(obs_left_l_, frenet_obstacle_sl.l_end);
-      obs_right_l_ = std::min(obs_right_l_, frenet_obstacle_sl.l_start);
-      obs_start_s_ = std::min(obs_start_s_, frenet_obstacle_sl.s_start);
-      obs_end_s_ = std::max(obs_end_s_, frenet_obstacle_sl.s_end);
-      static_blocked_obj_vec_.emplace_back(obstacle->obstacle()->id());
+        break;
     }
+
+    // if (left_borrow_ && right_borrow_) {
+    //   if (obs_bypass_direction == bypass_direction_) {
+    //     obs_left_l_ = std::max(obs_left_l_, frenet_obstacle_sl.l_end);
+    //     obs_right_l_ = std::min(obs_right_l_, frenet_obstacle_sl.l_start);
+    //     obs_start_s_ = std::min(obs_start_s_, frenet_obstacle_sl.s_start);
+    //     obs_end_s_ = std::max(obs_end_s_, frenet_obstacle_sl.s_end);
+    //     static_blocked_obj_vec_.emplace_back(obstacle->obstacle()->id());
+    //   } else {
+    //     break;
+    //   }
+    // } else {
+    //   obs_left_l_ = std::max(obs_left_l_, frenet_obstacle_sl.l_end);
+    //   obs_right_l_ = std::min(obs_right_l_, frenet_obstacle_sl.l_start);
+    //   obs_start_s_ = std::min(obs_start_s_, frenet_obstacle_sl.s_start);
+    //   obs_end_s_ = std::max(obs_end_s_, frenet_obstacle_sl.s_end);
+    //   static_blocked_obj_vec_.emplace_back(obstacle->obstacle()->id());
+    // }
   }
 
   if (obs_left_l_ <= obs_right_l_) {
