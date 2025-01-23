bb54e7f0b62f3549f58b0d555d14b39f8ab609ff
gdbai2 <gdbai2@iflytek.com>
2025-01-23 17:53:56 +0800

    fix: fix bug in narrow update remaining dist

--- src/modules/apa_function/parking_scenario/narrow_space/narrow_space_scenario.h
+++ src/modules/apa_function/parking_scenario/narrow_space/narrow_space_scenario.h
@@ -60,7 +60,10 @@ class NarrowSpaceScenario : public ParkingScenario {
 
   const bool CheckStuckFailed() override;
 
-  void UpdateRemainDist(const double uss_safe_dist) override;
+  void UpdateRemainDist(
+      const double uss_safe_dist,
+      const double lat_buffer = apa_param.GetParam().lat_inflation,
+      const double extra_buffer_when_reversing = 0.068) override;
 
   const std::string GetPlanReason(const uint8_t type);
 
