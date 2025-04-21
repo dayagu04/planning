f4e350eb4e38e932c23497ae6d73b3fca9f773c0
jwliu23 <jwliu23@iflytek.com>
2025-04-21 10:57:35 +0800

    fix: lc headway bug

--- src/modules/tasks/behavior_planners/agent_headway_decider/agent_headway_decider.cc
+++ src/modules/tasks/behavior_planners/agent_headway_decider/agent_headway_decider.cc
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "agent/agent.h"
+#include "config/basic_type.h"
 #include "debug_info_log.h"
 #include "environmental_model.h"
 #include "log.h"
@@ -57,7 +58,8 @@ bool AgentHeadwayDecider::UpdateAgentsHeadwayInfos() {
       session_->planning_context().lane_change_decider_output();
   const auto lane_change_state = lane_change_decider_output.curr_state;
   const bool is_in_lane_change_execution =
-      lane_change_state == kLaneChangeExecution;
+      lane_change_state == kLaneChangeExecution ||
+      lane_change_state == kLaneChangeComplete;
   const auto* st_graph_helper = session_->planning_context().st_graph_helper();
   const auto& dynamic_world =
       session_->environmental_model().get_dynamic_world();
