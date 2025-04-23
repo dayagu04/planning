2642c6bb0f40a651231bd1161868cc59dfab84c4
ywwang33 <ywwang33@iflytek.com>
2025-04-23 10:41:32 +0800

    apa: when slant slot angle bigger than a value, force perpendicular slot

--- jupyter_pybind/notebooks_apa/scripts/plot_tune_apa_simulation.py
+++ jupyter_pybind/notebooks_apa/scripts/plot_tune_apa_simulation.py
@@ -21,7 +21,7 @@ from struct_msgs.msg import PlanningOutput, UssPerceptInfo, GroundLinePerception
 # e0y-9:  18049
 # e0y-10: 20267
 # bag path and frame dt
-bag_path = '/data_cold/abu_zone/autoparse/chery_e0y_18049/trigger/20250417/20250417-15-24-12/park_in_data_collection_CHERY_E0Y_18049_ALL_FILTER_2025-04-17-15-25-12_no_camera.bag'
+bag_path = '/data_cold/abu_zone/autoparse/chery_e0y_18049/trigger/20250408/20250408-15-00-46/park_in_data_collection_CHERY_E0Y_18049_ALL_FILTER_2025-04-08-15-00-46_no_camera.bag'
 
 frame_dt = 0.1 # sec
 parking_flag = True
