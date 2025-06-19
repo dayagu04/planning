fc7829f44892d0497f176a947347e41aad5659fb
haoliu25 <haoliu25@iflytek.com>
2025-06-19 19:05:23 +0800

    [fix] :fix lat_buffer_outside for head out

--- src/modules/apa_function/parking_scenario/narrow_space/narrow_space_scenario.cc
+++ src/modules/apa_function/parking_scenario/narrow_space/narrow_space_scenario.cc
@@ -2272,6 +2272,11 @@ const bool NarrowSpaceScenario::CheckDynamicParkingIn() {
 }
 
 const bool NarrowSpaceScenario::CheckDynamicHeadOut() {
+  if (apa_world_ptr_->GetStateMachineManagerPtr()->GetParkOutDirection() ==
+      ApaParkOutDirection::FRONT) {
+    // This direction does not require dynamic planning;
+    return false;
+  }
   const ApaParameters& param = apa_param.GetParam();
   const bool car_motion_flag =
       !apa_world_ptr_->GetMeasureDataManagerPtr()->GetStaticFlag();
