07265836537d31dacd3543dd93f796dde2b17ea7
dhli13 <dhli13@iflytek.com>
2025-07-26 13:01:30 +0800

    update landing point

--- src/modules/tasks/trajectory_generator/result_trajectory_generator.cc
+++ src/modules/tasks/trajectory_generator/result_trajectory_generator.cc
@@ -5,6 +5,8 @@
 #include <vector>
 
 #include "environmental_model.h"
+#include "modules/tasks/task_interface/lane_change_decider_output.h"
+#include "geometry_math.h"
 
 // #include "core/common/trace.h"
 #include "debug_info_log.h"
@@ -62,6 +64,7 @@ bool ResultTrajectoryGenerator::Execute() {
   bool res = false;
   res = TrajectoryGenerator();
   UpdateTurnSignal();
+  UpdateHMIInfo();
   auto end_time = IflyTime::Now_ms();
   JSON_DEBUG_VALUE("TrajectoryGeneratorCostTime", end_time - start_time);
   return res;
@@ -379,6 +382,289 @@ void ResultTrajectoryGenerator::UpdateTurnSignal() {
                                       : RequestType::RIGHT_CHANGE;
     return;
   }
+}
+
+void ResultTrajectoryGenerator::UpdateHMIInfo() {
+  const auto &lane_change_decider_output =
+      session_->planning_context().lane_change_decider_output();
+  const auto &route_info_output =
+      session_->environmental_model().get_route_info()->get_route_info_output();
+  auto &ad_info = session_->mutable_planning_context()
+                      ->mutable_planning_hmi_info()
+                      ->ad_info;
+  const auto &ego_state_manager =
+      session_->environmental_model().get_ego_state_manager();
+  ad_info.cruise_speed = ego_state_manager->ego_v_cruise();
+  ad_info.lane_change_direction =
+      (iflyauto::LaneChangeDirection)lane_change_decider_output.lc_request;
+  // update LaneChangeStatus
+  const auto curr_state = lane_change_decider_output.curr_state;
+  const auto lasr_frame_state = session_->planning_context()
+                                    .lane_change_decider_output()
+                                    .coarse_planning_info.source_state;
+  static double lc_complete_to_lk_time = 0.0;
+  if (curr_state == kLaneKeeping) {
+    if (lasr_frame_state == kLaneChangeComplete) {
+      lc_complete_to_lk_time = IflyTime::Now_ms();
+      ad_info.lane_change_status =
+          iflyauto::LaneChangeStatus::LC_STATE_COMPLETE;
+    } else {
+      ad_info.lane_change_status =
+          iflyauto::LaneChangeStatus::LC_STATE_NO_CHANGE;
+
+      if (session_->is_hpp_scene()) {
+        // for HPP turn signal road to ramp
+        const auto hpp_turn_signal = lane_change_decider_output.hpp_turn_signal;
+        if (hpp_turn_signal == NO_CHANGE) {
+          ad_info.lane_change_status =
+              iflyauto::LaneChangeStatus::LC_STATE_NO_CHANGE;
+        } else if (hpp_turn_signal == LEFT_CHANGE) {
+          ad_info.lane_change_direction =
+              iflyauto::LaneChangeDirection::LC_DIR_LEFT;
+        } else if (hpp_turn_signal == RIGHT_CHANGE) {
+          ad_info.lane_change_direction =
+              iflyauto::LaneChangeDirection::LC_DIR_RIGHT;
+        }
+      } else {
+        // for NOA turn signal road to ramp
+        const auto dir_turn_signal_road_to_ramp =
+            lane_change_decider_output.dir_turn_signal_road_to_ramp;
+        if (dir_turn_signal_road_to_ramp == RAMP_NONE) {
+          ad_info.lane_change_status =
+              iflyauto::LaneChangeStatus::LC_STATE_NO_CHANGE;
+        } else if (dir_turn_signal_road_to_ramp == RAMP_ON_LEFT) {
+          ad_info.lane_change_direction =
+              iflyauto::LaneChangeDirection::LC_DIR_LEFT;
+        } else if (dir_turn_signal_road_to_ramp == RAMP_ON_RIGHT) {
+          ad_info.lane_change_direction =
+              iflyauto::LaneChangeDirection::LC_DIR_RIGHT;
+        }
+      }
+    }
+  } else if (curr_state == kLaneChangePropose) {
+    ad_info.lane_change_status = iflyauto::LaneChangeStatus::LC_STATE_WAITING;
+  } else if (curr_state == kLaneChangeExecution) {
+    ad_info.lane_change_status = iflyauto::LaneChangeStatus::LC_STATE_STARTING;
+  } else if (curr_state == kLaneChangeComplete) {
+    ad_info.lane_change_status = iflyauto::LaneChangeStatus::LC_STATE_STARTING;
+  } else if (curr_state == kLaneChangeCancel || curr_state == kLaneChangeHold) {
+    ad_info.lane_change_status = iflyauto::LaneChangeStatus::LC_STATE_CANCELLED;
+  }
+
+  // update StatusUpdateReason
+  const auto int_request_cancel_reason =
+      lane_change_decider_output.int_request_cancel_reason;
+  const auto lc_invalid_reason = lane_change_decider_output.lc_invalid_reason;
+  const auto lc_back_reason = lane_change_decider_output.lc_back_reason;
+  if (int_request_cancel_reason == SOLID_LC &&
+      lane_change_decider_output.lc_request_source == INT_REQUEST) {
+    ad_info.status_update_reason =
+        iflyauto::StatusUpdateReason::STATUS_UPDATE_REASON_SOLID_LINE;
+    // 暂时为了满足实线变道时打灯合planing_hmi的提示需求
+    // 在此更新变道状态和变道方向的值！！！！！！！
+    //  TODO(fengwang31):在变道过程中，遇到实线取消了，是否需要发出方向？
+    ad_info.lane_change_direction =
+        (iflyauto::LaneChangeDirection)
+            lane_change_decider_output.ilc_virtual_req;
+    ad_info.lane_change_status = iflyauto::LaneChangeStatus::LC_STATE_NO_CHANGE;
+  } else if (int_request_cancel_reason == MANUAL_CANCEL) {
+    ad_info.status_update_reason =
+        iflyauto::StatusUpdateReason::STATUS_UPDATE_REASON_MANUAL_CANCEL;
+  } else if (lc_invalid_reason == "side view invalid" ||
+             lc_invalid_reason == "front view invalid" ||
+             lc_back_reason == "side view back" ||
+             lc_back_reason == "front view back" ||
+             lc_back_reason == "but back cnt below threshold") {
+    ad_info.status_update_reason =
+        iflyauto::StatusUpdateReason::STATUS_UPDATE_REASON_SIDE_VEH;
+    iflyauto::ObstacleInfo obstacle;
+    obstacle.id = lane_change_decider_output.lc_invalid_track.track_id;
+    ad_info.obstacle_info[0] = obstacle;
+  } else {
+    ad_info.status_update_reason =
+        iflyauto::StatusUpdateReason::STATUS_UPDATE_REASON_NONE;
+  }
+
+  // update LaneChangeReason
+  const auto lc_request_source = lane_change_decider_output.lc_request_source;
+  if (lc_request_source == NO_REQUEST) {
+    ad_info.lane_change_reason = iflyauto::LaneChangeReason::LC_REASON_NONE;
+  } else if (lc_request_source == INT_REQUEST) {
+    ad_info.lane_change_reason = iflyauto::LaneChangeReason::LC_REASON_MANUAL;
+  } else if (lc_request_source == OVERTAKE_REQUEST) {
+    ad_info.lane_change_reason =
+        iflyauto::LaneChangeReason::LC_REASON_SLOWING_VEH;
+  } else if (lc_request_source == MAP_REQUEST) {
+    if (route_info_output.dis_to_ramp <
+        route_info_output.distance_to_first_road_merge) {
+      ad_info.lane_change_reason = iflyauto::LaneChangeReason::LC_REASON_SPLIT;
+    } else {
+      ad_info.lane_change_reason = iflyauto::LaneChangeReason::LC_REASON_MERGE;
+    }
+  } else if (lc_request_source == MERGE_REQUEST) {
+    ad_info.lane_change_reason = iflyauto::LaneChangeReason::LC_REASON_MERGE;
+  }
+  // update route info
+  if (!route_info_output.is_on_ramp) {
+    ad_info.distance_to_ramp = route_info_output.dis_to_ramp;
+  } else {
+    ad_info.distance_to_ramp = NL_NMAX;
+  }
+  ad_info.distance_to_split = route_info_output.distance_to_first_road_split;
+  if (route_info_output.is_ramp_merge_to_road_on_expressway) {
+    ad_info.distance_to_merge = route_info_output.distance_to_first_road_merge;
+  } else {
+    ad_info.distance_to_merge = NL_NMAX;
+  }
+  ad_info.distance_to_toll_station = route_info_output.distance_to_toll_station;
+  ad_info.noa_exit_warning_level_distance =
+      route_info_output.distance_to_route_end;
+  // ad_info.distance_to_tunnel = ;  //
+  // ad_info.is_within_hdmap = ;     //
+  const int ramp_direction = route_info_output.ramp_direction;
+  ad_info.ramp_direction = (iflyauto::RampDirection)ramp_direction;
+
+  const auto &fix_reference_path =
+      lane_change_decider_output.coarse_planning_info.reference_path;
+  if (fix_reference_path != nullptr) {
+    ad_info.dis_to_reference_line =
+        std::abs(fix_reference_path->get_frenet_ego_state().l() * 100);
+    ad_info.angle_to_roaddirection =
+        fix_reference_path->get_frenet_ego_state().heading_angle();
+  }
+
+  ad_info.is_in_sdmaproad = route_info_output.is_in_sdmaproad;
+  if (route_info_output.is_ego_on_expressway_hmi) {
+    ad_info.road_type = iflyauto::DrivingRoadType::DRIVING_ROAD_TYPE_HIGHWAY;
+    // update RampPassSts
+    // 高速主路上距离匝道距离小于200m，且不在一分二车道的场景
+    if (route_info_output.dis_to_ramp < 200 &&
+        lane_change_decider_output.dir_turn_signal_road_to_ramp == RAMP_NONE &&
+        !route_info_output.is_on_ramp) {
+      if (ramp_direction == iflyauto::RAMP_LEFT &&
+          !lane_change_decider_output.is_ego_on_leftmost_lane) {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_READYTOMISS;
+      } else if (ramp_direction == iflyauto::RAMP_RIGHT &&
+                 !lane_change_decider_output.is_ego_on_rightmost_lane) {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_READYTOMISS;
+      } else {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_NONE;
+      }
+    } else {
+      ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_NONE;
+    }
+  } else if (route_info_output.is_ego_on_city_expressway_hmi) {
+    ad_info.road_type = iflyauto::DrivingRoadType::DRIVING_ROAD_TYPE_OVERPASS;
+    // update RampPassSts
+    // 城区主路上距离匝道距离小于50m，且不在一分二车道的场景
+    if (route_info_output.dis_to_ramp < 50 &&
+        lane_change_decider_output.dir_turn_signal_road_to_ramp == RAMP_NONE &&
+        !route_info_output.is_on_ramp) {
+      if (ramp_direction == iflyauto::RAMP_LEFT &&
+          !lane_change_decider_output.is_ego_on_leftmost_lane) {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_READYTOMISS;
+      } else if (ramp_direction == iflyauto::RAMP_RIGHT &&
+                 !lane_change_decider_output.is_ego_on_rightmost_lane) {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_READYTOMISS;
+      } else {
+        ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_NONE;
+      }
+    } else {
+      ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_NONE;
+    }
+  } else {
+    ad_info.road_type = iflyauto::DrivingRoadType::DRIVING_ROAD_TYPE_NONE;
+    ad_info.ramp_pass_sts = iflyauto::RAMP_PASS_STS_NONE;
+  }
+  ad_info.landing_point.heading = 0.0;
+  ad_info.landing_point.relative_pos.x = 0.0;
+  ad_info.landing_point.relative_pos.y = 0.0;
+  ad_info.landing_point.relative_pos.z = 0.0;
+  JSON_DEBUG_VALUE("ramp_pass_sts", (int)ad_info.ramp_pass_sts)
+  double lc_complete_time_period = IflyTime::Now_ms() - lc_complete_to_lk_time;
+  static constexpr double kMaxTimeToCompleteLaneChange = 5000.0;  // ms
+  bool is_lane_keeping = curr_state == kLaneKeeping;
+  bool is_time_out = lc_complete_time_period > kMaxTimeToCompleteLaneChange;
+  iflyauto::LandingPoint landing_point;
+  if (!is_time_out && is_lane_keeping) {
+    landing_point = CalculateLandingPoint(true, lane_change_decider_output);
+  } else if (curr_state == kLaneChangePropose || curr_state == kLaneChangeExecution ||
+      curr_state == kLaneChangeComplete || curr_state == kLaneChangeCancel ||
+      curr_state == kLaneChangeHold) {
+    landing_point = CalculateLandingPoint(false, lane_change_decider_output);
+    // }
+  } else {
+    landing_point = CalculateLandingPoint(true, lane_change_decider_output);
+  }
+  ad_info.landing_point = landing_point;
+  return;
+}
+
+iflyauto::LandingPoint ResultTrajectoryGenerator::CalculateLandingPoint(
+    bool is_lane_keeping,
+    const LaneChangeDeciderOutput &lane_change_decider_output) {
+  iflyauto::LandingPoint landing_point;
+  landing_point.relative_pos.x = 0.0;
+  landing_point.relative_pos.y = 0.0;
+  landing_point.relative_pos.z = 0.0;
+  landing_point.heading = 0.0;
+  std::shared_ptr<ReferencePath> target_reference = nullptr;
+  double l_velocity = 1.4;
+  auto ego_state_manager =
+      session_->environmental_model().get_ego_state_manager();
+  double ego_v = std::fmax(1.0, ego_state_manager->ego_v_cruise() * 0.2);
+  double l_min = 0.15;
+  if (is_lane_keeping) {
+    target_reference = session_->environmental_model()
+                           .get_reference_path_manager()
+                           ->get_reference_path_by_current_lane();
+
+  } else {
+    int target_reference_virtual_id =
+        lane_change_decider_output.target_lane_virtual_id;
+    target_reference =
+        session_->environmental_model()
+            .get_reference_path_manager()
+            ->get_reference_path_by_lane(target_reference_virtual_id, false);
+  }
+  if (target_reference != nullptr) {
+    double l = std::fabs(target_reference->get_frenet_ego_state().l());
+    // double t_ratio = std::fabs(l) / std::fabs(l_max);
+    double t = std::fmax(0.0, l - l_min) / l_velocity;
+
+    if (l < l_min) {
+      t = 0.0;
+    }
+
+    Point2D cart_point;
+    if (!target_reference->get_frenet_coord()->SLToXY(
+            Point2D(target_reference->get_frenet_ego_state().s() + t * ego_v,
+                    0),
+            cart_point)) {
+      return landing_point;
+    }
+    const auto &ego_pose = ego_state_manager->ego_pose();
+    const double theta_ori = ego_pose.theta;
+    double landing_point_theta_global = 0;
+    ReferencePathPoint reference_path_point{};
+    if (target_reference->get_reference_point_by_lon(
+            target_reference->get_frenet_ego_state().s() + t * ego_v,
+            reference_path_point)) {
+      landing_point_theta_global = reference_path_point.path_point.theta();
+    }
+    Eigen::Vector2d pos_n_ori(ego_pose.x, ego_pose.y);
+    pnc::geometry_lib::GlobalToLocalTf global_to_local_tf(pos_n_ori, theta_ori);
+    Eigen::Vector2d p_n(cart_point.x, cart_point.y);
+    Eigen::Vector2d landing_point_body = global_to_local_tf.GetPos(p_n);
+    const double landing_point_theta_local =
+        global_to_local_tf.GetHeading(landing_point_theta_global);
+
+    landing_point.relative_pos.x = landing_point_body.x();
+    landing_point.relative_pos.y = landing_point_body.y();
+    landing_point.relative_pos.z = 0;
+    landing_point.heading = landing_point_theta_local;
+  }
+  return landing_point;
 }
 
 }  // namespace planning
--- src/modules/tasks/trajectory_generator/result_trajectory_generator.h
+++ src/modules/tasks/trajectory_generator/result_trajectory_generator.h
@@ -7,9 +7,8 @@
 // #include "core/modules/common/config/basic_types.h"
 #include "task_basic_types.h"
 #include "tasks/task.h"
-
+#include "tasks/task_interface/lane_change_decider_output.h"
 namespace planning {
-
 class ResultTrajectoryGenerator : public Task {
  public:
   explicit ResultTrajectoryGenerator(
@@ -23,6 +22,8 @@ class ResultTrajectoryGenerator : public Task {
   bool RealtimeTrajectoryGenerator();
 
   void Init();
+  void UpdateHMIInfo();
+  void UpdateTurnSignal();
 
   void UpdateTurnSignal();
 
@@ -39,6 +40,12 @@ class ResultTrajectoryGenerator : public Task {
   std::vector<double> ddkappa_vec_;
   std::vector<double> lat_acc_vec_;
   std::vector<double> lat_jerk_vec_;
+
+  iflyauto::LandingPoint CalculateLandingPoint(
+      bool is_lane_keeping,
+      const LaneChangeDeciderOutput& lane_change_decider_output);
+
+ private:
   ResultTrajectoryGeneratorConfig config_;
 };
 
