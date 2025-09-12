a5a432541301789f0550e37b9145bc8207f6aae3
thzhang5 <thzhang5@iflytek.com>
2025-09-12 16:06:57 +0800

    [dev]: use sd map speed limit

--- src/modules/adas_function/tsr_function/tsr_core.cc
+++ src/modules/adas_function/tsr_function/tsr_core.cc
@@ -388,11 +388,11 @@ void TsrCore::UpdateTsrSpeedLimit(void) {
   // sd_map信息，获取限速
   if (GetContext.get_session()->environmental_model()
                                 .get_route_info()
-                                ->get_sdpromap_valid()) {
-    const auto &sd_pro_map_info_ptr = GetContext.get_session()
+                                ->get_sdmap_valid()) {
+    const auto &sd_map_info_ptr = GetContext.get_session()
                                   ->environmental_model()
                                   .get_route_info()
-                                  ->get_sdpro_map();
+                                  ->get_sd_map();
     
     // 获取当前道路限速值
     // ego_motion信息
@@ -406,7 +406,7 @@ void TsrCore::UpdateTsrSpeedLimit(void) {
     double temp_nearest_s = 0;
     double nearest_l = 0;
     const double ego_heading_angle = localization_info->heading_angle();
-    const iflymapdata::sdpro::LinkInfo_Link* current_link = sd_pro_map_info_ptr.GetNearestLinkWithHeading(
+    const SdMapSwtx::Segment* current_link = sd_map_info_ptr.GetNearestRoadWithHeading(
         current_point, search_distance, ego_heading_angle, max_heading_diff,
         temp_nearest_s, nearest_l);
     if (!current_link) {
@@ -587,24 +587,25 @@ void TsrCore::UpdateTsrSpeedLimitOnlyByMap(void) {
   // sd_map信息，获取限速
   if (GetContext.get_session()->environmental_model()
                                 .get_route_info()
-                                ->get_sdpromap_valid()) {
-    const auto &sd_pro_map_info_ptr = GetContext.get_session()
+                                ->get_sdmap_valid()) {
+    const auto &sd_map_info_ptr = GetContext.get_session()
                                   ->environmental_model()
                                   .get_route_info()
-                                  ->get_sdpro_map();
+                                  ->get_sd_map();
     
     // 获取当前道路限速值
+    // ego_motion信息
     auto localization_info = GetContext.mutable_session()->mutable_environmental_model()->
-                              get_ego_state_manager();
+                              get_ego_state_manager(); // enu实际上是boot
     ad_common::math::Vec2d current_point;
     current_point.set_x(localization_info->location_enu().position.x);
-    current_point.set_y(localization_info->location_enu().position.y);
+    current_point.set_y(localization_info->location_enu().position.y); // enu实际上是boot
     const double search_distance = 50.0;
     const double max_heading_diff = PI / 4;
     double temp_nearest_s = 0;
     double nearest_l = 0;
     const double ego_heading_angle = localization_info->heading_angle();
-    const iflymapdata::sdpro::LinkInfo_Link* current_link = sd_pro_map_info_ptr.GetNearestLinkWithHeading(
+    const SdMapSwtx::Segment* current_link = sd_map_info_ptr.GetNearestRoadWithHeading(
         current_point, search_distance, ego_heading_angle, max_heading_diff,
         temp_nearest_s, nearest_l);
     
