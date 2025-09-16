cd9f3dfc2d62f78198b322b0608ace6e1de1ea49
thzhang5 <thzhang5@iflytek.com>
2025-09-16 14:37:46 +0800

    Revert "[dev]: use sd map speed limit"
    This reverts commit a5a432541301789f0550e37b9145bc8207f6aae3.

--- src/modules/adas_function/tsr_function/tsr_core.cc
+++ src/modules/adas_function/tsr_function/tsr_core.cc
@@ -587,13 +587,28 @@ void TsrCore::UpdateTsrSpeedLimitOnlyByMap(void) {
   // sd_map信息，获取限速
   if (GetContext.get_session()->environmental_model()
                                 .get_route_info()
-                                ->get_sdmap_valid()) {
-    const auto &sd_map_info_ptr = GetContext.get_session()
+                                ->get_sdpromap_valid()) {
+    const auto &sd_pro_map_info_ptr = GetContext.get_session()
                                   ->environmental_model()
                                   .get_route_info()
-                                  ->get_sd_map();
+                                  ->get_sdpro_map();
     
-    if (sd_map_info_ptr.GetNaviRoadInfo() == std::nullopt) {
+    // 获取当前道路限速值
+    auto localization_info = GetContext.mutable_session()->mutable_environmental_model()->
+                              get_ego_state_manager();
+    ad_common::math::Vec2d current_point;
+    current_point.set_x(localization_info->location_enu().position.x);
+    current_point.set_y(localization_info->location_enu().position.y);
+    const double search_distance = 50.0;
+    const double max_heading_diff = PI / 4;
+    double temp_nearest_s = 0;
+    double nearest_l = 0;
+    const double ego_heading_angle = localization_info->heading_angle();
+    const iflymapdata::sdpro::LinkInfo_Link* current_link = sd_pro_map_info_ptr.GetNearestLinkWithHeading(
+        current_point, search_distance, ego_heading_angle, max_heading_diff,
+        temp_nearest_s, nearest_l);
+    
+    if (!current_link) {
       tsr_speed_limit_ = 0;
       tsr_speed_limit_valid_ = false;
       current_map_speed_limit_valid_ = false;
