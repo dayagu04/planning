6333116fa48c0f57fa892f6d0ee4aec9855d8fe8
xykuai <xykuai@iflytek.com>
2025-09-17 22:42:15 +0800

    add ilc surpression in intersection

--- src/modules/tasks/behavior_planners/lane_change_decider/lane_change_requests/interactive_lane_change_request.cc
+++ src/modules/tasks/behavior_planners/lane_change_decider/lane_change_requests/interactive_lane_change_request.cc
@@ -41,6 +41,12 @@ void IntRequest::Update(int lc_status) {
     return;
   }
 
+  // intersection ilc surpression
+  if (EgoInIntersection()) {
+    finish_and_clear();
+    return;
+  }
+
   const double ego_v =
       session_->environmental_model().get_ego_state_manager()->ego_v();
 
