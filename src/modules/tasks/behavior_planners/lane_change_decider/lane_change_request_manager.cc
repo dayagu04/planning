5e846a6ba01cdaf4f45fa0441176deac7f27f825
clzhao9 <clzhao9@iflytek.com>
2025-10-09 17:15:18 +0800

    reset req when target lane bullptr

--- src/modules/tasks/behavior_planners/lane_change_decider/lane_change_request_manager.cc
+++ src/modules/tasks/behavior_planners/lane_change_decider/lane_change_request_manager.cc
@@ -382,6 +382,8 @@ bool LaneChangeRequestManager::Update(int lc_status, const bool hd_map_valid) {
                << target_lane_order_id
                << " target_lane_virtual_id:" << target_lane_virtual_id_;
   } else {
+    request_ = NO_CHANGE;
+    request_source_ = NO_REQUEST;
     target_lane_virtual_id_ = virtual_lane_mgr_->current_lane_virtual_id();
     ILOG_DEBUG << "[LCRequestManager::update] Target lane lost !!! final "
                   "target_lane_virtual_id:"
