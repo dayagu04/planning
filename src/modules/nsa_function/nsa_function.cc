9439d8789a3030210b6a9e97f8d308c703a0c832
clren <clren@iflytek.com>
2025-10-20 10:14:07 +0800

    add nsa taskpipeline

---
+++ src/modules/nsa_function/nsa_function.cc
@@ -0,0 +1,19 @@
+#include "hpp_function.h"
+
+#include "src/hpp_task_pipeline.h"
+
+namespace planning {
+NsaFunction::NsaFunction(framework::Session *session) : BaseFunction(session) {
+  auto config_builder = session->environmental_model().nsa_config_builder();
+  task_pipeline_ = std::make_unique<NsaTaskPipeline>(config_builder, session);
+}
+
+bool NsaFunction::Reset() {
+  task_pipeline_.reset(nullptr);
+
+  return true;
+}
+
+bool NsaFunction::Plan() { return task_pipeline_->Run(); }
+
+}  // namespace planning
