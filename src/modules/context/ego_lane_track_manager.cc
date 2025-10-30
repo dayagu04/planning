39cf710a6b8f9630ebfc7b21d548bfaf59d9ff54
xykuai <xykuai@iflytek.com>
2025-10-30 17:00:08 +0800

    update noa split selector

--- src/modules/context/ego_lane_track_manager.cc
+++ src/modules/context/ego_lane_track_manager.cc
@@ -166,11 +166,9 @@ void EgoLaneTrackManger::TrackEgoLane(
       }
       if (function_info.function_mode() == common::DrivingFunctionInfo::NOA) {
         const auto &first_static_split_region_info = mlc_decider_route_info.first_static_split_region_info;
-        double ego_dis_to_split_exchange_area_start = NL_NMAX;
-        if (first_static_split_region_info.is_valid) {
-          ego_dis_to_split_exchange_area_start = first_static_split_region_info.distance_to_split_point +
+        double ego_dis_to_split_exchange_area_start = 
+            first_static_split_region_info.distance_to_split_point +
               first_static_split_region_info.start_fp_point.fp_distance_to_split_point;
-        }
         if (!is_in_lane_borrow_status && ego_in_split_region_ && ego_dis_to_split_exchange_area_start < kSplitSelectEgoToExchangeEreaDistanceThd) {
           bool is_on_road_select_ramp = CheckIfInRoadSelectRampForSdpro(
               relative_id_lanes, order_ids_of_same_zero_relative_id);
@@ -220,6 +218,17 @@ void EgoLaneTrackManger::TrackEgoLane(
             if (is_exist_split_on_expressway_) {
               return;
             }
+          }
+        }
+
+        if (ego_in_split_region_ && ego_dis_to_split_exchange_area_start >= kSplitSelectEgoToExchangeEreaDistanceThd) {
+          ProcessIntersectionSplit(relative_id_lanes,
+                                   order_ids_of_same_zero_relative_id);
+          ILOG_DEBUG << "EgoLaneTrackManger::is_exist_split_on_intersection:"
+                     << is_exist_split_on_intersection_;
+
+          if (is_exist_split_on_intersection_) {
+            return;
           }
         }
 
