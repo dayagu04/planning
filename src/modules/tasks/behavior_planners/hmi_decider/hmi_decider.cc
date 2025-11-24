d4e4ff8abd51770cd4a4ffa8f37a5cde20ba34b7
dhli13 <dhli13@iflytek.com>
2025-11-24 17:54:18 +0800

    protect nullptr

--- src/modules/tasks/behavior_planners/hmi_decider/hmi_decider.cc
+++ src/modules/tasks/behavior_planners/hmi_decider/hmi_decider.cc
@@ -6,18 +6,27 @@ HMIDecider::HMIDecider(const EgoPlanningConfigBuilder* config_builder,
     : Task(config_builder, session) {
   name_ = "HMIDecider";
   config_ = config_builder->cast<HmiDeciderConfig>();
-  cone_warning_hmi_decider_ =
-      std::make_shared<ConeWarningHMIDecider>(session);
-  construction_warning_hmi_decider_ = std::make_shared<ConstructionWarningHMIDecider>(session);
+  cone_warning_hmi_decider_ = std::make_shared<ConeWarningHMIDecider>(session);
+  construction_warning_hmi_decider_ =
+      std::make_shared<ConstructionWarningHMIDecider>(session);
   lane_change_hmi_decider_ = std::make_shared<LaneChangeHmiDecider>(session);
-  longitudinal_hmi_decider_ = std::make_shared<LongitudinalHmiDecider>(session, config_);
+  longitudinal_hmi_decider_ =
+      std::make_shared<LongitudinalHmiDecider>(session, config_);
 }
 
 bool HMIDecider::Execute() {
-  cone_warning_hmi_decider_->Execute();
-  construction_warning_hmi_decider_->Execute();
-  lane_change_hmi_decider_->Execute();
-  longitudinal_hmi_decider_->Execute();
+  if (cone_warning_hmi_decider_) {
+    cone_warning_hmi_decider_->Execute();
+  }
+  if (construction_warning_hmi_decider_) {
+    construction_warning_hmi_decider_->Execute();
+  }
+  if (lane_change_hmi_decider_) {
+    lane_change_hmi_decider_->Execute();
+  }
+  if (longitudinal_hmi_decider_) {
+    longitudinal_hmi_decider_->Execute();
+  }
   return true;
 }
 }  // namespace planning
