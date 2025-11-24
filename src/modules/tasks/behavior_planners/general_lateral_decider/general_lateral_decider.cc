f8313ecf2635ede2781bbab8d9a599ac4e06c173
huwang5 <huwang5@iflytek.com>
2025-11-24 16:09:27 +0800

    fix fix_ExtractBoundary

--- src/modules/tasks/behavior_planners/general_lateral_decider/general_lateral_decider.cc
+++ src/modules/tasks/behavior_planners/general_lateral_decider/general_lateral_decider.cc
@@ -3848,14 +3848,14 @@ void GeneralLateralDecider::ExtractBoundary(
       if (first_soft_bound.second < second_frenet_soft_bounds[i].first) {
         first_soft_bound.second = second_frenet_soft_bounds[i].first;
       }
-      if (first_soft_bound.first > second_frenet_soft_bounds[i].second) {
-        if (second_soft_bounds_info[i].second.type == BoundType ::LANE ||
-            second_soft_bounds_info[i].second.type ==
-                BoundType ::EGO_POSITION) {
-          second_frenet_soft_bounds[i].second = first_soft_bound.first;
-        } else {
-          first_soft_bound.first = second_frenet_soft_bounds[i].second;
-        }
+    }
+    if (first_soft_bound.first > second_frenet_soft_bounds[i].second) {
+      if (second_soft_bounds_info[i].second.type == BoundType ::LANE ||
+          second_soft_bounds_info[i].second.type ==
+              BoundType ::EGO_POSITION) {
+        second_frenet_soft_bounds[i].second = first_soft_bound.first;
+      } else {
+        first_soft_bound.first = second_frenet_soft_bounds[i].second;
       }
     }
     if (!(second_soft_bounds_info[i].second.type == BoundType ::ROAD_BORDER ||
@@ -3874,13 +3874,13 @@ void GeneralLateralDecider::ExtractBoundary(
       if (first_soft_bound.first > second_frenet_soft_bounds[i].second) {
         first_soft_bound.first = second_frenet_soft_bounds[i].second;
       }
-      if (first_soft_bound.second < second_frenet_soft_bounds[i].first) {
-        if (second_soft_bounds_info[i].first.type == BoundType ::LANE ||
-            second_soft_bounds_info[i].first.type == BoundType ::EGO_POSITION) {
-          second_frenet_soft_bounds[i].first = first_soft_bound.second;
-        } else {
-          first_soft_bound.second = second_frenet_soft_bounds[i].first;
-        }
+    }
+    if (first_soft_bound.second < second_frenet_soft_bounds[i].first) {
+      if (second_soft_bounds_info[i].first.type == BoundType ::LANE ||
+          second_soft_bounds_info[i].first.type == BoundType ::EGO_POSITION) {
+        second_frenet_soft_bounds[i].first = first_soft_bound.second;
+      } else {
+        first_soft_bound.second = second_frenet_soft_bounds[i].first;
       }
     }
     first_frenet_soft_bounds[i] = first_soft_bound;
