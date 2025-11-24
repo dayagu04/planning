4bf7e986ad8071fa74600bd80c8f211acb50c0b7
hwtang3 <hwtang3@iflytek.com>
2025-11-24 10:54:34 +0800

    Fix: fix follow logic

--- src/modules/tasks/behavior_planners/long_ref_path_decider/target_marker/follow_target.h
+++ src/modules/tasks/behavior_planners/long_ref_path_decider/target_marker/follow_target.h
@@ -6,8 +6,6 @@
 #include "lon_target_maker.pb.h"
 #include "session.h"
 #include "target.h"
-#include "trajectory1d/second_order_time_optimal_trajectory.h"
-#include "trajectory1d/variable_coordinate_time_optimal_trajectory.h"
 
 namespace planning {
 
@@ -39,46 +37,15 @@ class FollowTarget : public Target {
 
   void MakeMinFollowDistance();
 
-  // generate stable curve
-  std::shared_ptr<VariableCoordinateTimeOptimalTrajectory>
-  GenerateStableFollowSlowCurve(const double matched_desired_headway,
-                                const bool enable_stable_jlt) const;
-
-  // generate far slow curve
-  std::shared_ptr<VariableCoordinateTimeOptimalTrajectory>
-  GenerateFarFollowSlowCurve(const bool enable_far_slow_jlt) const;
-
-  VariableCoordinateTimeOptimalTrajectory MakeSafeFarSlowCurve(
-      const CoordinateParam& relative_coordinate_param) const;
-
-  bool IsSafeFarSlowCurve(
-      const VariableCoordinateTimeOptimalTrajectory& far_slow_curve) const;
-
-  bool GenerateRelativeCoordinate(
-      const double follow_time_gap,
-      CoordinateParam* const relative_coordinate_param) const;
-
-  bool GenerateFarSlowCarRelativeCoordinate(
-      const int64_t st_boundary_id, const double follow_time_gap,
-      CoordinateParam* const relative_coordinate_param) const;
-
   bool MakeSValueWithTargetFollowCurve(const int32_t index,
                                        const bool has_valid_s_value,
                                        double* const target_s_value) const;
-
-  bool JudgeStableCar(const double matched_desired_headway) const;
-
-  bool JudgeFarSlowCar(const double matched_desired_headway) const;
-
-  bool JudgeSrefValid(
-      std::shared_ptr<VariableCoordinateTimeOptimalTrajectory> jlt_curve) const;
 
   void AddFollowTargetDataToProto();
 
  private:
   std::vector<UpperBoundInfo> upper_bound_infos_;
   planning::common::FollowTarget follow_target_pb_;
-  bool enable_target_follow_curve_ = false;
   double min_follow_distance_m_ = 3.0;
 };
 
