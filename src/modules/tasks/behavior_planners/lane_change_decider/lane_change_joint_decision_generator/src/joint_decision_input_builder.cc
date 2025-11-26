aad519fc1452b49c6abbe946444e687d433879a5
hwtang3 <hwtang3@iflytek.com>
2025-11-26 17:26:06 +0800

    Add: add lane_change acc

--- src/modules/tasks/behavior_planners/lane_change_decider/lane_change_joint_decision_generator/src/joint_decision_input_builder.cc
+++ src/modules/tasks/behavior_planners/lane_change_decider/lane_change_joint_decision_generator/src/joint_decision_input_builder.cc
@@ -24,7 +24,7 @@ JointDecisionInputBuilder::JointDecisionInputBuilder(
   comfort_params_.v0 = 33.5;
   comfort_params_.s0 = 2.5;
   comfort_params_.T = lc_decision_config_.lc_thw;
-  comfort_params_.a = 1.5;
+  comfort_params_.a = 2.0;
   comfort_params_.b_max = 2.0;
   comfort_params_.b = 1.0;
   comfort_params_.b_hard = 4.0;
