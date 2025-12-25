08b154f162b7d469aab6cca98b3e22aaa43bc180
clzhao9 <clzhao9@iflytek.com>
2025-12-25 14:18:00 +0800

    add front large obs buff

--- src/modules/tasks/behavior_planners/lane_change_decider/lane_change_state_machine_manager.cc
+++ src/modules/tasks/behavior_planners/lane_change_decider/lane_change_state_machine_manager.cc
@@ -4203,7 +4203,7 @@ bool LaneChangeStateMachineManager::
           (-rel_vel * ego_trajs_future_[i].v) / (2.0 * ego_brake);
       box_longitudinal_buff = std::max(3.5, box_longitudinal_buff);
       if (is_large_car) {
-        box_longitudinal_buff += 3.0;  // 大车额外增加3m基础距离
+        box_longitudinal_buff += 5.0;  // 大车额外增加5m基础距离
       }
       if (ego_press_line_ratio > 0.01 && is_side_clear_) {//对侧方车保持返回能力
         break;  // 已经压线以后，不再检查前车安全性，压线后再变道返回对前车是危险的。
