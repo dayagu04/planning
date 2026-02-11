1707140d04b8833bdeb77ec039f4271faff0d8fe
gpxu <gpxu@iflytek.com>
2026-02-11 14:41:07 +0800

    opt rads rear obs filter logic

--- src/modules/common/st_graph/st_graph_utils.cc
+++ src/modules/common/st_graph/st_graph_utils.cc
@@ -1131,8 +1131,7 @@ bool StGraphUtils::CalculateSRange(
                               ->get_vehicle_param()
                               .front_edge_to_rear_axle;
     if (agent.is_stop_destination_virtual_obs() == true ||
-        agent.type() > agent::AgentType::OCC_EMPTY ||
-        agent.type() < agent::AgentType::OCC_EMPTY) {
+        agent.type() > agent::AgentType::OCC_EMPTY) {
       *lower_s = min_s - front_edge_to_center;
       *upper_s = max_s + back_edge_to_center;
       if (agent.is_stop_destination_virtual_obs() == true && *lower_s < 0.0) {
