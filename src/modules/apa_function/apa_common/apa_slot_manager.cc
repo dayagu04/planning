70e8984503e8b2e03134eba1388a0346c5e24fb5
taolu10 <taolu10@iflytek.com>
2026-03-05 00:17:51 +0800

    [E541][HPP] disable apa path plan in hpp cruise

--- src/modules/apa_function/apa_common/apa_slot_manager.cc
+++ src/modules/apa_function/apa_common/apa_slot_manager.cc
@@ -420,6 +420,8 @@ void ApaSlotManager::GenerateReleaseSlotIdVec() {
     return;
   }
 
+  bool is_hpp_cruise = state_machine_ptr_->IsHppCruise();
+
   for (const auto& pair : dist_id_map_) {
     if (slots_map_.count(pair.second) == 0) {
       continue;
@@ -442,7 +444,7 @@ void ApaSlotManager::GenerateReleaseSlotIdVec() {
 
     bool is_slot_release = false;
 
-    if (slot.id_ != ego_info_under_slot_.id) {
+    if (is_hpp_cruise || slot.id_ != ego_info_under_slot_.id) {
       is_slot_release = true;
     } else {
       const SlotReleaseInfo& ego_release_info =
