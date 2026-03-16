39981866e83e0a279b840edc9bfea9689c15ff3a
gpxu <gpxu@iflytek.com>
2026-03-16 20:57:11 +0800

    intersection tfl red stop reminder opt

--- src/modules/tasks/behavior_planners/hmi_decider/longitudinal_hmi/longitudinal_hmi_decider.cc
+++ src/modules/tasks/behavior_planners/hmi_decider/longitudinal_hmi/longitudinal_hmi_decider.cc
@@ -19,8 +19,22 @@ bool LongitudinalHmiDecider::Execute() {
   const auto tfl_manager =
       session_->environmental_model().get_traffic_light_decision_manager();
   const auto traffic_status = tfl_manager->GetTrafficStatus();
+  bool is_red_tfl = traffic_status.go_straight == 1 || traffic_status.go_straight == 41 ||
+                    traffic_status.go_straight == 11 || traffic_status.go_straight == 10;
   const auto& tfl_decider = session_->mutable_planning_context()
                                 ->mutable_traffic_light_decider_output();
+
+  double dis_to_stopline = session_->environmental_model()
+                               .get_virtual_lane_manager()
+                               ->GetEgoDistanceToStopline();
+  double dis_to_crosswalk = session_->environmental_model()
+                               .get_virtual_lane_manager()
+                               ->GetEgoDistanceToCrosswalk();
+  const auto& ego_vehi_param =
+               VehicleConfigurationContext::Instance()->get_vehicle_param();
+  const double ego_rear_axle_to_front_edge =
+               ego_vehi_param.length - ego_vehi_param.rear_edge_to_rear_axle;
+
   planning::common::IntersectionState intersection_state =
       session_->environmental_model()
           .get_virtual_lane_manager()
@@ -34,12 +48,11 @@ bool LongitudinalHmiDecider::Execute() {
   constexpr uint8 kIntersectionStatusNone = 8;
   ad_info.intersection_pass_sts =
       iflyauto::IntersectionPassSts(kIntersectionStatusNone);
-  if (!tfl_decider.can_pass && intersection_state ==
-       planning::common::APPROACH_INTERSECTION &&
-      (!tfl_decider.is_small_front_intersection ||
-       tfl_decider.is_tfl_match_intersection) &&
-      (cipv_info.cipv_id() == -1 ||
-       cipv_info.relative_s() > config_.tfl_reminder_cipv_dis)) {
+  if (is_red_tfl && !tfl_decider.can_pass && intersection_state ==
+      planning::common::APPROACH_INTERSECTION && (!tfl_decider.is_small_front_intersection ||
+      tfl_decider.is_tfl_match_intersection) && (cipv_info.cipv_id() == -1 ||
+      (cipv_info.relative_s() + ego_rear_axle_to_front_edge > dis_to_stopline + config_.tfl_reminder_cipv_dis ||
+       cipv_info.relative_s() + ego_rear_axle_to_front_edge > dis_to_crosswalk + config_.tfl_reminder_cipv_dis))) {
     ad_info.intersection_pass_sts =
         iflyauto::IntersectionPassSts::INTERSECTION_RED_LIGHT_STOP;
   }
