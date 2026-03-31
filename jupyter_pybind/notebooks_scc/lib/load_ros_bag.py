97d4af4d395674c1b6f6edff8c0ca7f69f53a679
zdjiang3 <zdjiang3@iflytek.com>
2026-03-31 09:57:04 +0800

    fix:graph err

--- jupyter_pybind/notebooks_scc/lib/load_ros_bag.py
+++ jupyter_pybind/notebooks_scc/lib/load_ros_bag.py
@@ -839,12 +839,7 @@ class LoadRosbag:
                          "ResultTrajectoryGeneratorTime","ParkingSwitchDeciderTime","ARAStarTime",'HPP turn signal','hpp_lon_collision_check_time_cost', \
                          "dist_to_target_slot", "dist_to_target_dest", "is_exist_target_slot", "is_target_slot_allowed_to_park",\
                          "is_standstill_near_target_slot", "is_timeout_for_target_slot_allowed_to_park", "current planning_success", "pass_interval_first", "pass_interval_second", "edt_manager_cost","GeneralLateralDeciderCostTime"]
-                        #  "v_limit_curv", "v_limit_avoid",\
-                        #  "ego_head_s", "ego_s",
-                        #  "v_limit_speed_bump", "in_speed_bump_zone", "approaching_speed_bump", "distance_to_bump_zone",
-                        #  "speed_bump_near_ignore_range_first", "speed_bump_near_ignore_range_second", "update_speed_bump_near_ignore_range",\
-                        #  "v_limit_speed_ramp", "in_speed_limit_ramp", "approaching_speed_limit_zone_ramp", "distance_to_zone_ramp", \
-                        #  "v_limit_speed_intersection_road", "in_speed_limit_intersection", "approaching_speed_limit_zone_intersection", "distance_to_zone_intersection"]
+
       json_vector_list += ["lon_collision_object_position_x_vec",
                            "lon_collision_object_position_y_vec",
                            "expand_num_vec"]
